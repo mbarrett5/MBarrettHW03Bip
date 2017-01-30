c1edc7c78f67a822675bda7c0f21823cf1b00725
Michael Barrett <MikeBarrett@michaels-macbook-pro.local>
2017-01-29 22:25:08 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Michael Barrett on 1/29/17.
+//  Copyright © 2017 Michael Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
